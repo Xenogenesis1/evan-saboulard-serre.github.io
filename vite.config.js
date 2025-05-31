a81a48376bed38311789091562cbfb8e8460549c
Xenogenesis1 <evan2004lol@gmail.com>
2025-05-31 13:41:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/evan-saboulard-serre.github.io/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
   server: {
     open: true
